a2e0b9610f2c98e003a8b15574a9d99cb5257002
Takuto-Miyoshi <miyoshi.taku10@gmail.com>
2020-10-07 14:22:24 +0900

    リファクタリング作業 ・クラス名の変更 ・クラスの宣言と定義部分をファイルに分割 ・共通で使用するヘッダーファイルをCommon.hに移動

---
+++ 01_1007/ClassPractice/ClassPractice/Common.h
@@ -0,0 +1,7 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#endif // !COMMON_H_
---
+++ 01_1007/ClassPractice/ClassPractice/Unit.h
@@ -0,0 +1,21 @@
+#ifndef UNIT_H_
+#define UNIT_H_
+
+class Unit {
+
+public:
+	Unit();
+	~Unit();
+
+public:
+	int m_hp,  // ‘Ì—Í
+		m_str, // UŒ‚—Í
+		m_mag, // –‚–@—Í
+		m_tec, // ‹Z
+		m_spd, // •q·«
+		m_def, // –hŒä—Í
+		m_luc, // ‰^
+		m_mde; // –‚–@–hŒä—Í
+};
+
+#endif // UNIT_H_
