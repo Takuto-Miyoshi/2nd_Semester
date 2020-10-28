ce8ecf692f3855e154b47dda7425500e00e79cdf
Takuto-Miyoshi <miyoshi.taku10@gmail.com>
2020-10-28 17:28:59 +0900

    第4回実習 - 3
    関数のデフォルト引数について
    ・書き方、使い方
    ・デフォルト引数にする際の注意
    ・使用例

---
+++ 04_1028/Practice04/Practice04_03_DefaultArgument/Practice04_03_DefaultArgument.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+
+// デフォルト引数
+// 関数の引数にはデフォルト値を設定することができる
+// デフォルト値が設定された引数は、関数の呼び出し時に省略することができる
+class Calculator {
+public:
+	int Add( int a, int b );
+	int Mul( int a, int b = 2 );
+	
+private:
+
+};
+
+int Calculator::Add( int a, int b ) {
+	return ( a + b );
+}
+
+int Calculator::Mul( int a , int b ) {
+	return ( a * b );
+}
+
+int main(){
+	Calculator calc;
+	std::cout << calc.Add( 10, 20 ) << std::endl;
+	std::cout << calc.Mul( 30 ) << std::endl;
+}
