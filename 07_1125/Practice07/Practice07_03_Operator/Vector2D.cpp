430989ffb6c7032fdb7a54f9fbe91fd0e4d500bc
Takuto-Miyoshi <miyoshi.taku10@gmail.com>
2020-11-25 15:36:03 +0900

    第7回その３
    Vector2Dに代入、算術、比較演算子のオーバーロードを実装してみる
    ただしそれぞれ１つづつ

---
+++ 07_1125/Practice07/Practice07_03_Operator/Vector2D.cpp
@@ -0,0 +1,53 @@
+﻿
+#include <iostream>
+
+#include "Vector2D.h"
+
+Vector2D::Vector2D() :x( 0.0f ), y( 0.0f ){
+
+}
+
+Vector2D::Vector2D( float x_, float y_ ) : x( x_ ), y( y_ ){
+
+}
+
+float Vector2D::GetLength() const{
+
+	return float( sqrt(( x * x ) + ( y * y )) );
+}
+
+Vector2D& Vector2D::operator = ( const Vector2D& vec ){
+
+	this->x = vec.x;
+	this->y = vec.y;
+	return *this;
+}
+
+// 左辺値へ計算して代入を行うので基本的には代入演算子と同じ型になる
+Vector2D& Vector2D::operator += ( const Vector2D& vec ){
+
+	this->x += vec.x;
+	this->y += vec.y;
+	return *this;
+}
+
+Vector2D Vector2D::operator + ( const Vector2D& vec ){
+
+	Vector2D ans;
+	ans.x = this->x + vec.x;
+	ans.y = this->y + vec.y;
+	return ans;
+}
+
+Vector2D Vector2D::operator + ( float val ){
+	
+	Vector2D ans;
+	ans.x = this->x + val;
+	ans.y = this->y + val;
+	return ans;
+}
+
+bool Vector2D::operator > ( const Vector2D& vec ) const{
+
+	return this->GetLength() > vec.GetLength();
+}
---
+++ 07_1125/Practice07/Practice07_03_Operator/Vector2D.h
@@ -0,0 +1,65 @@
+﻿
+#ifndef VECTOR_2D_H
+#define VECTOR_2D_H
+
+/*
+算術演算子 + - * / % のオペレーター
+書き方
+	戻り値の型 operator +(const 参照引数)
+
+クラスのオペレーターとして用意する場合
+	Vector2D operator +(const Vector2D &vec);
+
+	戻り値の型
+		Vector2D			クラスの値渡し
+
+	引数
+		const Vector2D& vec	右辺のデータ型はconst参照型
+
+	呼び出し方の例
+		Vector2D vec1;
+		Vector2D vec2;
+
+		vec1 + vec2; この時に operator+ が呼び出される
+
+用意するメリット
+	・クラス内の全メンバ変数に対して、同じ計算をする場合にコーディングを短縮できる
+	・直感的に使うことができる
+	通常ならば AddPos などの関数を用意することが必要になる
+
+注意点
+	・Vector2Dの x, y に対して個別に値を加算、取得したい場合は通常通りに AddPosX, GetPosX などの関数を用意する必要がある
+	operator + だけでは X に足すのか Y に足すのか判断できないため
+*/
+
+// 二次元ベクトルクラス
+class Vector2D{
+public:
+	Vector2D();
+	Vector2D( float x_, float y_ );
+
+	float GetX() const { return x; }
+	float GetY() const { return y; }
+
+	float GetLength() const;
+
+	// = のオーバーロード
+	Vector2D& operator = ( const Vector2D& vec );
+
+	// += のオーバーロード
+	Vector2D& operator += ( const Vector2D& vec );
+
+	// + のオペレーターオーバーロード
+	Vector2D operator + ( const Vector2D& vec );
+
+	Vector2D operator + ( float val );
+
+	// > のオーバーロード
+	bool operator > ( const Vector2D& vec )const;
+
+private:
+	float x;
+	float y;
+};
+
+#endif // !VECTOR_2D_H
