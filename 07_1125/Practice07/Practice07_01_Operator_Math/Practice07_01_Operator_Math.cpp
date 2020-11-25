d3c4b273a0d2946e1ad7c8c451422d2586c4b32d
Takuto-Miyoshi <miyoshi.taku10@gmail.com>
2020-11-25 13:41:36 +0900

    第7回その１
    算術演算子のオーバーロード実践
    代入演算子のオーバーロード実践

--- 07_1125/Practice07/Practice07_01_Operator_Math/Practice07_01_Operator_Math.cpp
+++ 07_1125/Practice07/Practice07_01_Operator_Math/Practice07_01_Operator_Math.cpp
@@ -1,8 +1,24 @@
 ﻿
 #include <iostream>
 
+#include "Vector2D.h"
+
+/*
+算術演算子は + - * / % など
+代入演算子を使用した += -= などもある
+*/
+
 int main()
 {
+	Vector2D vec1( 100, 100 );
+	Vector2D vec2( 87, 14 );
+	Vector2D ans;
+
+	ans = vec1;
+	ans += vec2;
+
+	std::cout << "ans.x = " << ans.GetX() << std::endl;
+	std::cout << "ans.y = " << ans.GetY() << std::endl;
 
 	return 0;
 }
---
+++ 07_1125/Practice07/Practice07_01_Operator_Math/Vector2D.cpp
@@ -0,0 +1,33 @@
+﻿
+#include "Vector2D.h"
+
+Vector2D::Vector2D() :x( 0.0f ), y( 0.0f ){
+
+}
+
+Vector2D::Vector2D( float x_, float y_ ) : x( x_ ), y( y_ ){
+
+}
+
+Vector2D& Vector2D::operator = ( const Vector2D& vec ){
+
+	this->x = vec.x;
+	this->y = vec.y;
+	return *this;
+}
+
+// 左辺値へ計算して代入を行うので基本的には代入演算子と同じ型になる
+Vector2D& Vector2D::operator += ( const Vector2D& vec ){
+
+	this->x += vec.x;
+	this->y += vec.y;
+	return *this;
+}
+
+Vector2D Vector2D::operator + ( const Vector2D& vec ){
+
+	Vector2D ans;
+	ans.x = this->x + vec.x;
+	ans.y = this->y + vec.y;
+	return ans;
+}
